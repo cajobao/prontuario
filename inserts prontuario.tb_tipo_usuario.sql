c826a88ec383ac5fa9cffefed7e18cdbf4fcabe1
Jacob Vilar Santana <cajobao@gmail.com>
2016-10-01 21:16:40 -0300

    Add files via upload

---
+++ inserts prontuario.tb_tipo_usuario.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO prontuario.tb_tipo_pessoa (nm_tipo_pessoa) 
+	VALUES ('Funcionário');
+INSERT INTO prontuario.tb_tipo_pessoa (nm_tipo_pessoa) 
+	VALUES ('Responsável');
+INSERT INTO prontuario.tb_tipo_pessoa (nm_tipo_pessoa) 
+	VALUES ('Paciente');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Administrador');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Assistente Social');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Nutricionista');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Farmacêutico');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Psicólogo');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Fisioterapeuta');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Terapeuta Ocupacional');
+INSERT INTO prontuario.tb_tipo_usuario (nm_tipo_usuario) 
+	VALUES ('Cuidador(a)');
